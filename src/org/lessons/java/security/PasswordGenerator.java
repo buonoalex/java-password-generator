d5484549bd01ce1ed1aa8d82013632bef9fde28b
buonoalex <loopatthecity@gmail.com>
2023-11-29 16:14:36 +0100

    add variable color and print the output password.Finish work

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -10,6 +10,7 @@ public class PasswordGenerator {
         //Add variable
         String nameUser;
         String surnameUser;
+        String favouriteColorUser;
         int yearsUser;
         int dayUser;
         int monthUser;
@@ -26,20 +27,23 @@ public class PasswordGenerator {
         System.out.print("Inserisci il mese in cui sei nato : ");
         monthUser = myScanBro.nextInt();
 
-        //Input user month
+        //Input user name
         System.out.print("Inserisci il tuo nome : ");
-        nameUser = myScanBro.nextLine();
+        nameUser = myScanBro.next();
 
-        //Input user month
+        //Input user surname
         System.out.print("Inserisci il tuo cognome : ");
-        surnameUser = myScanBro.nextLine();
+        surnameUser = myScanBro.next();
+
+        //Input user color
+        System.out.print("Inserisci il tuo colore preferito : ");
+        favouriteColorUser = myScanBro.next();
+
+        //Sum year/month/day
+        int sumOfDate = monthUser+dayUser+yearsUser;
 
         //Generate the secure password
-
-
-
-
-
+        System.out.println("La tua super password è : "+nameUser+"-"+surnameUser+"-"+favouriteColorUser+"-"+sumOfDate);
 
         //Close the Scanner
         myScanBro.close();
