15eef2567fab5840455e2af38ab60958f49d2af1
buonoalex <loopatthecity@gmail.com>
2023-11-29 16:03:07 +0100

    open the scanner and add variable

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,48 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
+        //Open the Scanner
+        Scanner myScanBro = new Scanner(System.in);
+
+        //Add variable
+        String nameUser;
+        String surnameUser;
+        int yearsUser;
+        int dayUser;
+        int monthUser;
+
+        //Input user year
+        System.out.print("Inserisci l'anno in cui sei nato : ");
+        yearsUser = myScanBro.nextInt();
+
+        //Input user day
+        System.out.print("Inserisci il giorno in cui sei nato : ");
+        dayUser = myScanBro.nextInt();
+
+        //Input user month
+        System.out.print("Inserisci il mese in cui sei nato : ");
+        monthUser = myScanBro.nextInt();
+
+        //Input user month
+        System.out.print("Inserisci il tuo nome : ");
+        nameUser = myScanBro.nextLine();
+
+        //Input user month
+        System.out.print("Inserisci il tuo cognome : ");
+        surnameUser = myScanBro.nextLine();
+
+        //Generate the secure password
+
+
+
+
+
+
+        //Close the Scanner
+        myScanBro.close();
 
     }
 }
